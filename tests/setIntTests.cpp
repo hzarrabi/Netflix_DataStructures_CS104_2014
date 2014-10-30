32133163f26e339b9e784f1044068e5141f7193c
Hooman Zarrabi <hzarrabi@usc.edu>
2014-10-30 00:00:24 -0700

    dsf

--- tests/setIntTests.cpp
+++ tests/setIntTests.cpp
@@ -149,3 +149,63 @@ catch(NoSuchElementException e){}
 
 
 }
+
+//intersection test 3
+TEST (setUnionTest, BasicCase) {
+ 
+
+  Set<int> set1;//first set
+  Set<int> set2;//second set
+
+  set1.add(453);//
+  set1.add(23522);
+  set1.add(29);//1
+  set1.add(196);
+  set1.add(3003240);//
+  set1.add(36);
+  set1.add(86);//
+  set1.add(1233323);
+  set1.add(55);//
+  set1.add(353);//
+  set1.add(463);
+
+  set2.add(342335);
+  set2.add(353);
+  set2.add(29);//1
+  set2.add(3003240);
+  set2.add(34534);
+  
+
+
+
+
+  Set<int> setFinal =set1.setIntersection(set2);//using set union
+  vector<int> v;
+  try
+  {
+    for(Set<int>::Iterator a = setFinal.begin(); a!=setFinal.end(); ++a)
+    {
+      v.push_back(*a);//copying from set to vector
+    }
+}
+
+catch(NoSuchElementException e){}
+
+  vector<int> v_sorted=MergeSort::sort(v);
+  /*
+  if ordered vector would have
+55
+70
+90
+300
+532
+1222
+
+  */
+  
+    EXPECT_EQ(v_sorted.at(0), 29);
+    EXPECT_EQ(v_sorted.at(1), 353);
+    EXPECT_EQ(v_sorted.at(2), 3003240);
+
+
+}
